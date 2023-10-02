0aea2312d0198192059b1281b51174699cb5a1cd
CiobanuMarius9 <ciobanu.marius.g@gmail.com>
2023-10-02 17:02:11 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,31 @@ import MovieList from "./components/MovieList/MovieList";
 import Fire from "./assets/fire.png";
 import Star from "./assets/glowing-star.png";
 import Party from "./assets/partying-face.png";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <main>
-        <MovieList type="popular" title="Popular" emoji={Fire} />
-        <MovieList type="top_rated" title="Top Rated" emoji={Star} />
-        <MovieList type="upcoming" title="Upcoming" emoji={Party} />
+        <Routes>
+          <Route
+            path="/"
+            element={<MovieList type="popular" title="Popular" emoji={Fire} />}
+          />
+          <Route
+            path="/top_rated"
+            element={
+              <MovieList type="top_rated" title="Top Rated" emoji={Star} />
+            }
+          />
+          <Route
+            path="/upcoming"
+            element={
+              <MovieList type="upcoming" title="Upcoming" emoji={Party} />
+            }
+          />
+        </Routes>
       </main>
     </div>
   );
